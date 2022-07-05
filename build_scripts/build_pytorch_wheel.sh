903eef4a2651c60f730862921a56b7dc5be5a4ef
Nelson Liu <nfliu@nelsonliu.me>
2022-07-05 16:50:14 -0700

    Fix unbound variable

--- build_scripts/build_pytorch_wheel.sh
+++ build_scripts/build_pytorch_wheel.sh
@@ -5,10 +5,10 @@ desired_python="$1"
 desired_cuda="$2"
 pytorch_version="$3"
 builder_revision="$4"
-docker_image="$5"
+docker_image=""
 
-
-if [ -n "${docker_image}" ]; then
+if [ -n "$5" ]; then
+  docker_image="$5"
   echo "Using docker image ${docker_image}"
 else
   echo "Docker image not supplied, using pytorch/manylinux-cuda${CUDA_VERSION_NO_DOT}"
